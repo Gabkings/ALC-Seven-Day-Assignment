d2a25239fd5eff04fc5bfbcd926d3c3a6a394591
unknown <gabworks51@gmail.com>
2018-07-01 02:25:24 +0300

    this is my seven day code assignment from ALC google

---
+++ sw.js
@@ -0,0 +1,21 @@
+let cacheName = 'cache-v1';
+let urlsToCache = [
+  '/currency-converter-ALC-7DaysOfCode-Challenge/',
+  './index.html',
+  './css/index.css',
+  './css/mdb.min.css',
+  './bitcoin-495995_640.jpg',
+  './index.js',
+];
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(urlsToCache))
+  );
+});
